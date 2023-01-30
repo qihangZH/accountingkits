8cbb5433e69360ef532222cc8e32c9e4e6a13bfd
QihangZhang <694499657@qq.com>
2023-01-30 15:45:20 +0800

    change demo to ipynb and set new setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 import setuptools
 
-# setuptools.setup(
-#
-# )
+setuptools.setup(
+    name='AccountingKits',
+    version='0.1.0.230130',
+    description='Accounting kits packages, especially for banking research',
+    author='Zhang Qihang',
+    author_email='694499657@qq.com',
+    packages=setuptools.find_packages()
+)
