1cef7ba917375c0ec5794a6b6d12fd73f0733146
QihangZhang <694499657@qq.com>
2023-01-30 17:19:00 +0800

    update

--- accountingkits/Stats/SummarizeT.py
+++ accountingkits/Stats/SummarizeT.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pandas as pd
+import warnings
 
 
 def summary_quantile_ser(arrser):
-    return pd.Series({
-        'Mean': np.nanmean(arrser),
-        'Sd': np.nanstd(arrser),
-        'p25': np.nanquantile(arrser, q=0.25),
-        'Median': np.nanmedian(arrser),
-        'p75': np.nanquantile(arrser, q=0.75)
-    })
+    try:
+        return pd.Series({
+            'Mean': np.nanmean(arrser),
+            'Sd': np.nanstd(arrser),
+            'p25': np.nanquantile(arrser, q=0.25),
+            'Median': np.nanmedian(arrser),
+            'p75': np.nanquantile(arrser, q=0.75)
+        })
+    except:
+        warnings.warn('Some Error in selfmade, use pd.describe instead',
+                      SyntaxWarning)
+        return pd.Series(arrser).describe()
+
