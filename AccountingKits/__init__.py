2525e8c5a4139c07351e85f2580905f62100acdf
QihangZhang <694499657@qq.com>
2023-01-30 15:29:54 +0800

    change demo to ipynb and set new setup.py

--- AccountingKits/__init__.py
+++ AccountingKits/__init__.py
@@ -4,4 +4,4 @@ from . import Stats
 from . import _BasicFunc
 
 __version__ = '0.1.0.230130'
-print('\033[34mAccounting Package Version: {}\033[0m'.format(__version__))
+print('\033[34mAccounting Kits Version: {}\033[0m'.format(__version__))
