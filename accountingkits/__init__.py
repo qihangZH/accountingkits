1629a664f96f497ad824047136f6ee89ca9ee268
QihangZhang <694499657@qq.com>
2023-02-01 10:10:37 +0800

    Change version2

--- accountingkits/__init__.py
+++ accountingkits/__init__.py
@@ -3,5 +3,5 @@ from . import OSKit
 from . import Stats
 from . import _BasicFunc
 
-__version__ = '0.1.0.230201_beta'
+__version__ = '0.1.0.230201_gamma'
 print('\033[34m accountingkits Version: {}\033[0m'.format(__version__))
