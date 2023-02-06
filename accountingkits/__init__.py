238b55f3e01dbd169173f6cf4a7e08fc5374debf
QihangZhang <694499657@qq.com>
2023-02-06 23:07:31 +0800

    update version

--- accountingkits/__init__.py
+++ accountingkits/__init__.py
@@ -5,7 +5,7 @@ from . import WaybackScraper
 from . import _BasicFunc
 
 
-__version__ = '0.1.1.230205_alpha'
+__version__ = '0.1.1.230206_alpha'
 print('\033[34m accountingkits Version: {}\033[0m'.format(__version__))
 
 """
