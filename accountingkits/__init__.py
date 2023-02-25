47b1c8a950a9eaa16cbdb8a8cc0ec82ccfeb2164
QihangZhang <694499657@qq.com>
2023-02-25 14:32:08 +0800

    update version

--- accountingkits/__init__.py
+++ accountingkits/__init__.py
@@ -6,7 +6,7 @@ from . import CrawlerApi
 from . import _BasicFunc
 
 
-__version__ = '0.1.1.230221_alpha'
+__version__ = '0.1.1.230225_alpha'
 # print('\033[34m accountingkits Version: {}\033[0m'.format(__version__))
 
 """
