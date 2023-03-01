b6cc632dfa966e911fbb4dac9c056be6eab170f3
QihangZhang <694499657@qq.com>
2023-03-01 21:33:31 +0800

    update comment

--- accountingkits/_BasicFunc/MultiprocessF.py
+++ accountingkits/_BasicFunc/MultiprocessF.py
@@ -3,9 +3,8 @@ import signal
 
 def threads_interrupt_initiator():
     """
-    Each pool process will execute this as part of its
-    initialization.
+    Function for multiprocessing.Pool(initializer=threads_interrupt_initiator())
+    Each pool process will execute this as part of its initialization.
     Use this to keep safe for multiprocessing...and gracefully interrupt by keyboard
     """
     signal.signal(signal.SIGINT, signal.SIG_IGN)
-    
