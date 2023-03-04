b246d9049ff70b0f3211862291e2afbd4b7f93f3
QihangZhang <694499657@qq.com>
2023-03-04 12:36:30 +0800

    update comment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from accountingkits import __version__ as ak_version
 setuptools.setup(
     name='accountingkits',
     version=ak_version,
-    description='accountingkits packages, especially for banking research',
+    description='The package which made for science research in accounting, Biz school',
     author='zhang qihang',
     author_email='694499657@qq.com',
     packages=setuptools.find_packages()
