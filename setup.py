1e9601ccd82b98de7d5b4b5fddc7fac5ad91d614
QihangZhang <694499657@qq.com>
2023-03-05 20:35:36 +0800

    update comment

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
 from accountingkits import __version__ as ak_version
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='accountingkits',
     version=ak_version,
     description='The package which made for science research in accounting, Biz school',
     author='zhang qihang',
     author_email='694499657@qq.com',
-    packages=setuptools.find_packages()
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
