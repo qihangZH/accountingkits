39fcfd93c6a592f8378fe40effc0cca3a0fec514
QihangZhang <694499657@qq.com>
2023-03-07 21:26:19 +0800

    setup.py -> setup.cfg

--- setup.py
+++ setup.py
@@ -1,33 +1,4 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='accountingkits',
-    description='The kit-package which made for accounting science research',
-    author='zhang qihang',
-    author_email='694499657@qq.com',
-    url='https://github.com/qihangZH/accountingkits',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    python_requires=">=3.7",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=['numpy',
-                      'pandas',
-                      'pathos',
-                      'requests',
-                      'python-Levenshtein',
-                      'thefuzz',
-                      'rapidfuzz',
-                      'sas7bdat',
-                      'nltk',
-                      'beautifulsoup4',
-                      'fake-useragent',
-                      'Cython']
-)
+if __name__ == '__main__':
+    setuptools.setup()
