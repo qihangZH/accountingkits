25a1fbba957d37af3ac75962b77450cac3d49948
QihangZhang <694499657@qq.com>
2023-09-17 22:12:54 +0800

    Update threshold Njit for it could easily run in Njit Parallel and faster in larger dataset.

--- accountingkits/_BasicTools/__init__.py
+++ accountingkits/_BasicTools/__init__.py
@@ -2,5 +2,6 @@ from . import UniversalBasicT
 from . import ArrayLikeT
 from . import DecoratorT
 from . import MultiprocessT
-from . import NjitT
+from . import MatrixLikeT
+from . import _numbapack
 # _BasicTools is a module which hierarchy under all other modules in this pack
